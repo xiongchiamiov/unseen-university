685bbb50e42e377236586081d3626fa34320d9da
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-09-14 15:20:21 -0700

    Only return script names in the index.
    Extra information is just a waste of bandwidth, and potentially queries.

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from api.models import Script
 
 @json_view
 def index(request):
-    return [script.humanReadable() for script in Script.objects.order_by('name')]
+    return [script.name for script in Script.objects.order_by('name')]
 
 @json_view
 def detail(request, script):
