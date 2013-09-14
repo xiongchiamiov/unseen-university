62e7bb9e8c251bf28732aadfc00052142f5a2a58
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-09-14 15:12:55 -0700

    Add script detail endpoint.

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from api import views
 
 urlpatterns = patterns('',
+    url(r'^scripts/(?P<script>[\w_-]+)', views.detail),
     url(r'^scripts/', views.index),
 )
 
--- api/views.py
+++ api/views.py
@@ -6,3 +6,7 @@ from api.models import Script
 def index(request):
     return [script.humanReadable() for script in Script.objects.order_by('name')]
 
+@json_view
+def detail(request, script):
+    return Script.objects.get(name=script).humanReadable()
+
